1d87108edc41c77030361a3ab1794a5befb2f7b6
Avigailbe <46314482+Avigailbe@users.noreply.github.com>
2019-04-08 09:30:16 +0300

    Add files via upload

--- Back Up Database (Full)_Job.sql
+++ Back Up Database (Full)_Job.sql
@@ -1,11 +1,11 @@
 USE [msdb]
 GO
 
-/****** Object:  Job [BackupForCasino.Subplan_1]    Script Date: 03-Apr-19 9:33:16 AM ******/
+/****** Object:  Job [FullBackupCasino]    Script Date: 08-Apr-19 9:31:37 AM ******/
 BEGIN TRANSACTION
 DECLARE @ReturnCode INT
 SELECT @ReturnCode = 0
-/****** Object:  JobCategory [Database Maintenance]    Script Date: 03-Apr-19 9:33:16 AM ******/
+/****** Object:  JobCategory [Database Maintenance]    Script Date: 08-Apr-19 9:31:37 AM ******/
 IF NOT EXISTS (SELECT name FROM msdb.dbo.syscategories WHERE name=N'Database Maintenance' AND category_class=1)
 BEGIN
 EXEC @ReturnCode = msdb.dbo.sp_add_category @class=N'JOB', @type=N'LOCAL', @name=N'Database Maintenance'
@@ -14,19 +14,20 @@ IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
 END
 
 DECLARE @jobId BINARY(16)
-EXEC @ReturnCode =  msdb.dbo.sp_add_job @job_name=N'BackupForCasino.Subplan_1', 
+EXEC @ReturnCode =  msdb.dbo.sp_add_job @job_name=N'FullBackupCasino', 
 		@enabled=1, 
-		@notify_level_eventlog=2, 
-		@notify_level_email=0, 
+		@notify_level_eventlog=0, 
+		@notify_level_email=2, 
 		@notify_level_netsend=0, 
 		@notify_level_page=0, 
 		@delete_level=0, 
-		@description=N'No description available.', 
+		@description=N'Creates a full DB backup once a day at 5am', 
 		@category_name=N'Database Maintenance', 
-		@owner_login_name=N'DESKTOP-QPSKFM5\Ben Tovim', @job_id = @jobId OUTPUT
+		@owner_login_name=N'DESKTOP-QPSKFM5\Ben Tovim', 
+		@notify_email_operator_name=N'AdminOp', @job_id = @jobId OUTPUT
 IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-/****** Object:  Step [Subplan_1]    Script Date: 03-Apr-19 9:33:17 AM ******/
-EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'Subplan_1', 
+/****** Object:  Step [CreateFullBackup]    Script Date: 08-Apr-19 9:31:37 AM ******/
+EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'CreateFullBackup', 
 		@step_id=1, 
 		@cmdexec_success_code=0, 
 		@on_success_action=1, 
@@ -35,13 +36,14 @@ EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'Subplan_
 		@on_fail_step_id=0, 
 		@retry_attempts=0, 
 		@retry_interval=0, 
-		@os_run_priority=0, @subsystem=N'SSIS', 
-		@command=N'/Server "$(ESCAPE_NONE(SRVR))" /SQL "Maintenance Plans\BackupForCasino" /set "\Package\Subplan_1.Disable;false"', 
+		@os_run_priority=0, @subsystem=N'TSQL', 
+		@command=N'usp_createFullBackup', 
+		@database_name=N'Casino', 
 		@flags=0
 IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
 EXEC @ReturnCode = msdb.dbo.sp_update_job @job_id = @jobId, @start_step_id = 1
 IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-EXEC @ReturnCode = msdb.dbo.sp_add_jobschedule @job_id=@jobId, @name=N'BackupForCasino.Back Up Database (Full)', 
+EXEC @ReturnCode = msdb.dbo.sp_add_jobschedule @job_id=@jobId, @name=N'DailyFullBackup5am', 
 		@enabled=1, 
 		@freq_type=4, 
 		@freq_interval=1, 
@@ -49,11 +51,11 @@ EXEC @ReturnCode = msdb.dbo.sp_add_jobschedule @job_id=@jobId, @name=N'BackupFor
 		@freq_subday_interval=0, 
 		@freq_relative_interval=0, 
 		@freq_recurrence_factor=0, 
-		@active_start_date=20190403, 
+		@active_start_date=20190408, 
 		@active_end_date=99991231, 
 		@active_start_time=50000, 
 		@active_end_time=235959, 
-		@schedule_uid=N'962fc648-6f76-4a24-a9ac-0beac6854582'
+		@schedule_uid=N'9539b598-97c6-4a97-8ac4-05d401cb5db6'
 IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
 EXEC @ReturnCode = msdb.dbo.sp_add_jobserver @job_id = @jobId, @server_name = N'(local)'
 IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
