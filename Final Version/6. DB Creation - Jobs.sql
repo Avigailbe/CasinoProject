9baa2a3b230f4789f133ffcdb54e5f80cc050998
Avigailbe <46314482+Avigailbe@users.noreply.github.com>
2019-04-13 21:52:03 +0300

    Add files via upload

--- Final Version/6. DB Creation - Jobs.sql
+++ Final Version/6. DB Creation - Jobs.sql
@@ -9,7 +9,6 @@ SET @filePath = 'D:\TCDBA\GitHub\TCDBA24\Projects\Final Project\PasswordsFromGit
 SET @SQL ='
 USE [msdb]
 GO
-
 /****** Object:  Job [GitPasswords]    Script Date: 29/03/2019 10:46:33 ******/
 BEGIN TRANSACTION
 DECLARE @ReturnCode INT
@@ -19,9 +18,7 @@ IF NOT EXISTS (SELECT name FROM msdb.dbo.syscategories WHERE name=N''[Uncategori
 BEGIN
 EXEC @ReturnCode = msdb.dbo.sp_add_category @class=N''JOB'', @type=N''LOCAL'', @name=N''[Uncategorized (Local)]''
 IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
-
 END
-
 DECLARE @jobId BINARY(16)
 EXEC @ReturnCode =  msdb.dbo.sp_add_job @job_name=N''GitPasswords'', 
 		@enabled=1, 
@@ -90,7 +87,6 @@ GOTO EndSave
 QuitWithRollback:
     IF (@@TRANCOUNT > 0) ROLLBACK TRANSACTION
 EndSave:
-
 GO'
 
 EXEC (@SQL)
@@ -1002,5 +998,72 @@ QuitWithRollback:
 EndSave:
 GO
 
+---- Create daily partition job
+USE [msdb]
+GO
 
+/****** Object:  Job [createDailyPartition]    Script Date: 13-Apr-19 9:54:40 PM ******/
+BEGIN TRANSACTION
+DECLARE @ReturnCode INT
+SELECT @ReturnCode = 0
+/****** Object:  JobCategory [[Uncategorized (Local)]]    Script Date: 13-Apr-19 9:54:41 PM ******/
+IF NOT EXISTS (SELECT name FROM msdb.dbo.syscategories WHERE name=N'[Uncategorized (Local)]' AND category_class=1)
+BEGIN
+EXEC @ReturnCode = msdb.dbo.sp_add_category @class=N'JOB', @type=N'LOCAL', @name=N'[Uncategorized (Local)]'
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+
+END
+
+DECLARE @jobId BINARY(16)
+EXEC @ReturnCode =  msdb.dbo.sp_add_job @job_name=N'createDailyPartition', 
+		@enabled=1, 
+		@notify_level_eventlog=0, 
+		@notify_level_email=2, 
+		@notify_level_netsend=0, 
+		@notify_level_page=0, 
+		@delete_level=0, 
+		@description=N'create daily partitions on master tables', 
+		@category_name=N'[Uncategorized (Local)]', 
+		@owner_login_name=N'DESKTOP-QPSKFM5\Ben Tovim', 
+		@notify_email_operator_name=N'AdminOp', @job_id = @jobId OUTPUT
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+/****** Object:  Step [creates daily partitions on casino masters]    Script Date: 13-Apr-19 9:54:41 PM ******/
+EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N'creates daily partitions on casino masters', 
+		@step_id=1, 
+		@cmdexec_success_code=0, 
+		@on_success_action=1, 
+		@on_success_step_id=0, 
+		@on_fail_action=2, 
+		@on_fail_step_id=0, 
+		@retry_attempts=0, 
+		@retry_interval=0, 
+		@os_run_priority=0, @subsystem=N'TSQL', 
+		@command=N'usp_createNewPartition', 
+		@database_name=N'Casino', 
+		@flags=0
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+EXEC @ReturnCode = msdb.dbo.sp_update_job @job_id = @jobId, @start_step_id = 1
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+EXEC @ReturnCode = msdb.dbo.sp_add_jobschedule @job_id=@jobId, @name=N'daily partitions at 6:30am', 
+		@enabled=1, 
+		@freq_type=4, 
+		@freq_interval=1, 
+		@freq_subday_type=1, 
+		@freq_subday_interval=0, 
+		@freq_relative_interval=0, 
+		@freq_recurrence_factor=0, 
+		@active_start_date=20190413, 
+		@active_end_date=99991231, 
+		@active_start_time=63000, 
+		@active_end_time=235959, 
+		@schedule_uid=N'6553f495-ff29-44e9-88ca-0965b436381e'
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+EXEC @ReturnCode = msdb.dbo.sp_add_jobserver @job_id = @jobId, @server_name = N'(local)'
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+COMMIT TRANSACTION
+GOTO EndSave
+QuitWithRollback:
+    IF (@@TRANCOUNT > 0) ROLLBACK TRANSACTION
+EndSave:
+GO
 
